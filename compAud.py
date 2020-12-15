aec874802c41c8aec8562bc6120b903190d58c9f
unknown <odedwolff@gmail.com>
2020-12-15 16:33:07 +0200

    made handling of files more generlized, a preperation for supporting more file formats

--- compAud.py
+++ compAud.py
@@ -4,7 +4,7 @@ import os
 outputFolder = "./output";
 
 
-supported_files = ["wav"]
+supported_files = ["wav", "wma", "m4a"]
 
 #    script makes a compressed MP3 version of current folder tree. the output will be placed under a local output folder. if the source has subfolder (on any 
 #    depth) the folders sub tree will copied, rooted udner the traget folder. each folder in the target tree will contain output files for files 
@@ -12,10 +12,10 @@ supported_files = ["wav"]
 #
 
 def compDir(outputRoot, pathFromInputRoot):
-	
 	for entry in os.scandir( pathFromInputRoot):
-		if entry.is_file() and entry.path.endswith(".wav"):
-			AudioSegment.from_wav(entry.path).export(outputFolder + entry.path[:-4] + ".mp3", format="mp3")
+		if entry.is_file():
+			convertFile(entry.path, outputFolder)
+					
 		else:
 			if entry.is_dir() and entry.path != outputFolder:
 				newFolderPath = outputRoot + entry.path
@@ -30,5 +30,18 @@ def convertTreeToMp3():
 	compDir(outputFolder, "./")
 		
 #convertAllWavToMp3s(.)
+
+
+def convertFile(srcFilePath, outputFolderRoot):
+	fileExt=srcFilePath[-3:]
+	if not fileExt in supported_files:
+		print("file format " + fileExt + " not supported")
+		return None 
+	prefix=srcFilePath.replace(" ", "_")[0:-4]
+	print("prefix =" + prefix)
+	buf = AudioSegment.from_file(srcFilePath, fileExt)
+	buf.export(outputFolder + prefix + ".mp3", format="mp3");
+	
+	
 
 convertTreeToMp3()
